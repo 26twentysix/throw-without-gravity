9abfa83b915ecaffe3e3454a3b92d347047d255b
twentysix <54219970+26twentysix@users.noreply.github.com>
2020-04-06 02:30:46 +0500

    сохранение значений в динамический список

--- source.cpp
+++ source.cpp
@@ -1,45 +1,49 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-int main() {
+struct Node {
+	double xPlane, xBody, endTime, time;
+	Node* next;
+};
+
+int main()
+{
 	setlocale(0, "");
-	int count = 0;
-	double x = 0;
-	double x0;
-	double v1;
-	double v2;
-	double t0;
-	double t = 0;
-	double t1;
-	double sumOfT = 0;
-	cout << "Введите скорость плоскости и через сколько секунд с начала движения плоскости было брошено тело: " << endl;
-	cin >> v1 >> t0;
-	x0 = x + (v1 * t0);
-	cout << "Введите скорость тела: " << endl;
-	cin >> v2;
-	while (x0 > x) {
-		t1 = t;
-		t = (x0 - x) / v2;
-		cout << "Начальная координата тела - " << x << ", плоскости - " << x0 << endl;
-		x = x0;
-		x0 += v1 * t;
-		sumOfT += t;
-		cout << "Тело переместиться к координате " << x << " за " << t << " секунд, за это же время плоскость переместится к коорднате " << x0 << endl;
-		if (t > t1) {
-			count--;
-		}
-		if (t < t1) {
-			count++;
-		}
-		if (count < -9) {
-			cout << "Тело никогда не столкнется с плоскостью ";
-			break;
-		}
-		if (abs(x0 - x) < 0.001) {
-			cout << "Тело столкнулось с плоскостью на координате " << x << " с общим временем полета " << sumOfT << " c после " << count << " приближений";
-			break;
-		}
+	double xPlane, xBody, strtTime = 10, bodySpeed = 11, planeSpeed = 10, endTime, time = 0;
+	xPlane = planeSpeed * (strtTime + time);
+	xBody = bodySpeed * time;
+	endTime = ((xPlane - xBody) / bodySpeed);
+	time = time + endTime;
+	Node* a;
+	Node* b;
+	a = new Node;
+	a->xPlane = xPlane;
+	a->xBody = xBody;
+	a->endTime = endTime;
+	a->time = time;
+	b = new Node;
+	a->next = b;
+	while ((xPlane - xBody) > 0.00001)
+	{
+		xPlane = planeSpeed * (strtTime + time);
+		xBody = bodySpeed * time;
+		endTime = ((xPlane - xBody) / bodySpeed);
+		time = time + endTime;
+		b->xPlane = xPlane;
+		b->xBody = xBody;
+		b->endTime = endTime;
+		b->time = time;
+		b->next = new Node;
+		b = b->next;
+		b->next = nullptr;
+	}
+	b = a;
+	while (a != nullptr && b->xPlane > 0)
+	{
+		cout << "Координата плоскости = " << b->xPlane << "\n" << "Координата тела = " << b->xBody << "\n" << "Тело достигнет плоскости через " << b->endTime << "сек" << "\n" << "Время, прошедшее с момента начала движения телом = " << b->time << endl;
+		a = b->next;
+		delete b;
+		b = a;
 	}
 }
